eeb14d4f641490525a871cb6641b00d727220f5f
Bryan Kangah <77281151+staplevrt@users.noreply.github.com>
2022-12-15 12:23:00 +0100

    architecture and first test

--- pokedex/src/core/gateways/pokemonGateway.ts
+++ pokedex/src/core/gateways/pokemonGateway.ts
@@ -4,4 +4,5 @@ import { CreatePokemonDTO } from '../dto/createPokemonDTO'
 export interface ProductGateway {
   listAll(): Promise<Array<Pokemon>>
   create(createPokemonDTO: CreatePokemonDTO): Promise<Pokemon>
+  listOne(id : number):Promise<Pokemon>
 }
